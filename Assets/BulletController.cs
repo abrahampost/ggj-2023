ee3d3762b1bcc870ae1682fa08ccf5e9816703de
Simon Post <simon.post.1999@gmail.com>
2023-02-03 23:17:07 -0500

    bullets disappear when they hit enemy tag

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -25,9 +25,16 @@ public class BulletController : MonoBehaviour
     }
 
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        Debug.Log(velocity);
         rb.velocity = velocity * Time.deltaTime * 100 * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
     }
 }
