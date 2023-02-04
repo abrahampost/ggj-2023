420d347eeedf3e698575cd082391ffcad71682ae
Logan <loganq@gmail.com>
2023-02-04 16:59:38 -0500

    messed some more with UI stuff

--- Assets/Scripts/ForestSelectorController.cs
+++ Assets/Scripts/ForestSelectorController.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class ForestSelectorController : MonoBehaviour
 {
-    public Texture2D cursorTexture;
-    
     void Start()
     {
-        UnityEngine.Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.ForceSoftware);
-    }
-
-    void OnMouseEnter() {
-        UnityEngine.Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.ForceSoftware);
+        
     }
 
     // Update is called once per frame
