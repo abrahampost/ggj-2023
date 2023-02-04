ba3fc737f5f6854250b5a5617035cdb9b09e5660
Simon Post <simon.post.1999@gmail.com>
2023-02-03 23:53:34 -0500

    now have cooldown on gun

--- Assets/scripts/WeaponsController.cs
+++ Assets/scripts/WeaponsController.cs
@@ -1,25 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class WeaponsController : MonoBehaviour
 {
+    [Header("Gun")]
     public GameObject bullet;
-    public float launchVelocity;
-    public string keyBind;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string gunKeyBind;
+    public float gunCooldown = 2.0f;
+    private bool isGunOnCooldown = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown(keyBind))
+        if (Input.GetButtonDown(gunKeyBind) && !isGunOnCooldown)
         {
             Instantiate(bullet, transform.position, transform.rotation);
+            isGunOnCooldown = true;
+            Task.Delay(TimeSpan.FromSeconds(gunCooldown)).ContinueWith((task) => isGunOnCooldown = false);
         }
     }
 }
