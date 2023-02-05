41dc519d9b6244d8842dcf1d960ad330c353c029
Logan <loganq@gmail.com>
2023-02-05 01:34:11 -0500

    HUD works in ForestLevel

--- Assets/UI/ForestLevelController.cs
+++ Assets/UI/ForestLevelController.cs
@@ -7,24 +7,52 @@ using UnityEngine.SceneManagement;
 public class ForestLevelController : MonoBehaviour
 {
     private UIDocument doc;
-    public int Health;
-    private VisualElement health;
+
+    GameState gameState;
+    public int healthBarNumber;
+    private VisualElement healthBarContainer;
+    private Label agility;
+    private Label constitution;
+    private Label health;
+    private Label speed;
+    private Label strength;
 
     private void Awake() {
         doc = GetComponent<UIDocument>();
+        gameState = GameObject.Find("GameState").GetComponent<GameState>();
 
-        health = doc.rootVisualElement.Q<VisualElement>("Health");
-        
+        healthBarContainer = doc.rootVisualElement.Q<VisualElement>("Health");
+        agility = doc.rootVisualElement.Q<Label>("agilityNumber");
+        constitution = doc.rootVisualElement.Q<Label>("conNumber");
+        health = doc.rootVisualElement.Q<Label>("healthNumber");
+        speed = doc.rootVisualElement.Q<Label>("speedNumber");
+        strength = doc.rootVisualElement.Q<Label>("strNumber");
+
+        var agilityNumber = gameState.GetComponentInChildren<PlayerStats>().rawAttributes.agility + gameState.GetComponentInChildren<PlayerStats>().agilityBuff;
+        agility.text = "" + agilityNumber;
+
+        var conNumber = gameState.GetComponentInChildren<PlayerStats>().rawAttributes.constitution + gameState.GetComponentInChildren<PlayerStats>().constitutionBuff;
+        constitution.text = "" + conNumber;
+
+        var healthNumber = gameState.GetComponentInChildren<PlayerStats>().rawAttributes.health + gameState.GetComponentInChildren<PlayerStats>().healthBuff;
+        health.text = "" + healthNumber;
+
+        var speedNumber = gameState.GetComponentInChildren<PlayerStats>().rawAttributes.speed + gameState.GetComponentInChildren<PlayerStats>().speedBuff;
+        speed.text = "" + speedNumber;
+
+        var strNumber = gameState.GetComponentInChildren<PlayerStats>().rawAttributes.strength + gameState.GetComponentInChildren<PlayerStats>().strengthBuff;
+        strength.text = "" + strNumber;
     }
 
     void Update() {
-        health.style.maxWidth = Health;
-        if (Health > 205) {
-            Health = 205;
-            health.style.maxWidth = Health;
-        } else if (Health < 0) {
-            Health = 0;
-            health.style.maxWidth = Health;
+        healthBarContainer.style.maxWidth = healthBarNumber;
+        var percentage = (healthBarNumber / 205) * 100;
+        if (percentage > 100) {
+            healthBarNumber = 205;
+            healthBarContainer.style.maxWidth = healthBarNumber;
+        } else if (healthBarNumber < 0) {
+            healthBarNumber = 0;
+            healthBarContainer.style.maxWidth = healthBarNumber;
         }
     }
     
