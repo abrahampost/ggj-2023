60950a7616a2af0cf309b3df849d2729e1191762
Denver Massey <denmas22@live.unc.edu>
2023-02-05 11:58:40 -0500

    added enemies of different colors, and added trees to biomes

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -248,4 +248,11 @@ public class EnemyController : MonoBehaviour
         }
 
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy") {
+            Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+        }
+    }
 }
